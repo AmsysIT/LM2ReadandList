89670ee63c3f14d1d20d97f320fd886603bc54cf
hank00111 <42414986+hank00111@users.noreply.github.com>
2023-03-24 13:27:58 +0800

    20220324-1
    Fix box barCode 2

--- LM2ReadandList/Main.Designer.cs
+++ LM2ReadandList/Main.Designer.cs
@@ -369,7 +369,7 @@
             this.tabPage1.Controls.Add(this.PrintCheckBox);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(427, 86);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "基本項目";
@@ -542,7 +542,7 @@
             this.tabPage2.Controls.Add(this.label5);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(427, 86);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "列印控制";
@@ -1151,7 +1151,7 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("新細明體", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.checkBox1.Location = new System.Drawing.Point(267, 72);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(92, 34);
             this.checkBox1.TabIndex = 55;
